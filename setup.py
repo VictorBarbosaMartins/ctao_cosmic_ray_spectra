679c96f660ac629c5a112965ac8d966caad272c2
Victor Barbosa Martins <victorbarbosamartins@yahoo.com.br>
2024-01-24 16:18:26 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ctao_cosmic_ray_spectra',
+    version='0.1',
+    packages=['ctao_cosmic_ray_spectra'],
+    install_requires=[
+        "numpy",
+        "astropy",
+        "importlib",
+        "scipy",
+    ],
+)
